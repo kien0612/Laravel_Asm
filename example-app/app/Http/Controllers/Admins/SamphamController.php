bd542b9efb04b04fad4cce57e309dd625717e201
doquangquyet <queytdo01698705281@gmail.com>
2024-07-31 21:25:47 +0700

    sua sm

--- example-app/app/Http/Controllers/Admins/SamphamController.php
+++ example-app/app/Http/Controllers/Admins/SamphamController.php
@@ -107,15 +107,69 @@ class SamphamController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $danhmucs= DB::table('danh_mucs')->get();
+        $sampham = DB::table('san_phams')->where('id_san_pham', $id)->first();
+        return view('admin.Sampham.edit', compact('sampham', 'danhmucs'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id_san_pham)
     {
-        //
+        $validate = $request->validate([
+            'ten_san_pham' => 'required','string','max:255',
+            'hinh_anh' => 'required|max:2048',
+            'gia' => 'required|numeric|min:0',
+            'so_luong' => 'required|integer|min:0',
+            'ngay_nhap' => 'required|date',
+            'mo_ta' => 'nullable|string|min:50',
+            'trang_thai' => 'required',
+            
+        ], [
+            'ten_san_pham.required' => 'Tên bị trống',
+            'ten_san_pham.string' => 'Tên phải là chữ',
+            'ten_san_pham.max' => 'Tên quá dài',
+    
+            'hinh_anh.required' => 'Hình ảnh bị trống',
+        
+            'hinh_anh.max' => 'Kích thước hình ảnh quá lớn (tối đa 2MB)',
+    
+            'gia.required' => 'Giá bị trống',
+            'gia.numeric' => 'Giá phải là số',
+            'gia.min' => 'Giá phải lớn hơn hoặc bằng 0',
+    
+            'so_luong.required' => 'Số lượng bị trống',
+            'so_luong.integer' => 'Số lượng phải là số nguyên',
+            'so_luong.min' => 'Số lượng phải lớn hơn hoặc bằng 0',
+    
+            'ngay_nhap.required' => 'Ngày nhập bị trống',
+            'ngay_nhap.date' => 'Ngày nhập phải là ngày hợp lệ',
+    
+            'mo_ta.string' => 'Mô tả phải là chữ',
+            'mo_ta.min' => 'Mô tả phải lớn hơn 50 kí từ với đủ seo sản phẩm',
+    
+            'trang_thai.required' => 'Trạng thái bị trống',
+        
+
+            ]);
+            if ($request->hasFile('hinh_anh')) {
+                // Nếu có đẩy hình ảnh
+                $filename = $request->file('hinh_anh')->store('uploads/sanpham', 'public');
+                // Xóa hình ảnh cũ
+                Storage::delete('public/'. DB::table('san_phams')->where('id_san_pham', $id_san_pham)->first()->hinh_anh);
+            }
+            DB::table('san_phams')->where('id_san_pham', $id_san_pham)->update([
+                'hinh_anh' =>$filename,
+                'ten_san_pham' => $request->input('ten_san_pham'),
+                'gia' => $request->input('gia'),
+                'so_luong' => $request->input('so_luong'),
+                'ngay_nhap' => $request->input('ngay_nhap'),
+                'mo_ta' => $request->input('mo_ta'),
+                'trang_thai' => $request->input('trang_thai'),
+                'id_danh_muc' => $request->input('id_danh_muc')
+                 ]);
+                return redirect()->route('admin.sampham.index')->with('success', 'Cập nhập sản phẩm là xong thành công.');
     }
 
     /**
--- example-app/resources/views/admin/Sampham/edit.blade.php
+++ example-app/resources/views/admin/Sampham/edit.blade.php
@@ -2,48 +2,71 @@
 @extends('admin.layout');
 
 @section('content')
-<h1>Cập Nhập mã khuyễn mãi</h1>
+<h1>Thêm mới Sản Phẩm</h1>
 <br>
-<form action="{{route('admin.sampham.update',$sampham->id_khuyen_mai)}}" method="POST">
-  @method("PUT")
-    @csrf
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Mã khuyễn mãi </label>
-      <input type="text" class="form-control"  placeholder="nhập name vào" name="ten_khuyen_mai" value="{{$sampham->ten_khuyen_mai}}">
-    </div>
-    @error('ten_khuyen_mai')
-    <div class="alert alert-danger"> {{$message}} </div>
-    @enderror
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Phần trắn khuyễn mãi </label>
-      <input type="number" class="form-control"  placeholder="nhập name vào" name="phan_tram_giam_gia" value="{{$sampham->phan_tram_giam_gia}}">
-    </div>
-    @error('phan_tram_giam_gia')
-    <div class="alert alert-danger"> {{$message}} </div>
-    @enderror
-    <div class="mb-3">
+<form action="{{ route('admin.sampham.update', $sampham->id_san_pham) }}" method="POST" enctype="multipart/form-data">
+  @method('PUT')
+  @csrf
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Tên Sản Phẩm</label>
+      <input type="text" class="form-control" placeholder="Nhập tên sản phẩm vào" name="ten_san_pham" value="{{ old('ten_san_pham', $sampham->ten_san_pham) }}">
+  </div>
+  @error('ten_san_pham')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Hình ảnh</label>
+      <input type="file" class="form-control" placeholder="Chọn hình ảnh mới" name="hinh_anh">
+      <p class="form-control"><img src="{{ Storage::url($sampham->hinh_anh) }}" width="100" height="100" alt=""></p>
+  </div>
+  @error('hinh_anh')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Giá</label>
+      <input type="number" class="form-control" placeholder="Nhập giá sản phẩm" name="gia" value="{{ old('gia', $sampham->gia) }}">
+  </div>
+  @error('gia')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Số Lượng</label>
+      <input type="number" class="form-control" placeholder="Nhập số lượng sản phẩm" name="so_luong" value="{{ old('so_luong', $sampham->so_luong) }}">
+  </div>
+  @error('so_luong')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Ngày Nhập Sản phẩm</label>
+      <input type="date" class="form-control" placeholder="Chọn ngày nhập sản phẩm" name="ngay_nhap" value="{{ old('ngay_nhap', $sampham->ngay_nhap) }}">
+  </div>
+  @error('ngay_nhap')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Mô tả</label>
-      <input type="text" class="form-control"  placeholder="nhập name vào" name="mo_ta" value="{{$sampham->mo_ta}}">
-    </div>
-    @error('mo_ta')
-    <div class="alert alert-danger"> {{$message}} </div>
-    @enderror
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Ngày bắt đầu</label>
-      <input type="date" class="form-control"  placeholder="nhập name vào" name="ngay_bat_dau" value="{{$sampham->ngay_bat_dau}}">
-    </div>
-    @error('ngay_bat_dau')
-    <div class="alert alert-danger"> {{$message}} </div>
-    @enderror
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Ngày kết thúc </label>
-      <input type="date" class="form-control"  placeholder="nhập name vào" name="ngay_ket_thuc" value="{{$sampham->ngay_ket_thuc}}">
-    </div>
-    @error('ngay_ket_thuc')
-    <div class="alert alert-danger"> {{$message}} </div>
-    @enderror
-   
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+      <textarea cols="30" class="form-control" rows="10" name="mo_ta">{{ old('mo_ta', $sampham->mo_ta) }}</textarea>
+  </div>
+  @error('mo_ta')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Trạng Thái đợi hàng</label>
+      <input type="number" class="form-control" placeholder="Nhập trạng thái đợi hàng" name="trang_thai" value="{{ old('trang_thai', $sampham->trang_thai) }}">
+  </div>
+  @error('trang_thai')
+  <div class="alert alert-danger"> {{$message}} </div>
+  @enderror
+  <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Danh mục sản phẩm</label>
+      <select name="id_danh_muc" class="form-control">
+          @foreach($danhmucs as $danhmuc)
+          <option value="{{ $danhmuc->id_danh_muc }}" {{ old('id_danh_muc', $sampham->id_danh_muc) == $danhmuc->id_danh_muc ? 'selected' : '' }}>
+              {{ $danhmuc->ten_danh_muc }}
+          </option>
+          @endforeach
+      </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 @endsection
-
